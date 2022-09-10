8e0168e5c5c1ffc9e617338f5246a49fae422a1d
fosterinc7 <adenoikiadeniyi0108@gmail.com>
2022-09-09 18:27:46 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entrypoint
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z' letter_++)
+	{
+		letter = tolower(letter);
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}	
