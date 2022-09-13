80eef54fe60cd9fca32179e30b9967ded070af99
fosterinc7 <adenoikiadeniyi0108@gmail.com>
2022-09-12 18:09:50 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - print if the number is postive, zero, or negative
  *
  * Description: using the main function
- *this program prints "and that piece of art is useful\" - Dora Korpar,2015-10-19"
+ *this program prints"and that piece of art s useful\"-Dora Korpar,2015-10-19"
  * Return: return 1
  */
 
