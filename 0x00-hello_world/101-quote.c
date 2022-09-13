0e714117403ba953ea371d88c6d062dc68b9ee54
fosterinc7 <adenoikiadeniyi0108@gmail.com>
2022-09-12 18:05:54 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - print if the number is postive, zero, or negative
  *
  * Description: using the main function
- * this program prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+ *this program prints "and that piece of art is useful\" - Dora Korpar,2015-10-19"
  * Return: return 1
  */
 
