73e109f51d8b8070a9de4445da9a9a21def2ecb5
fosterinc7 <adenoikiadeniyi0108@gmail.com>
2022-09-22 20:37:54 -0700

    experiment

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,9 +13,18 @@ void print_number(int n)
 		_putchar('-');
 		num = -num;
 	}
+	if (n == 0)
+		_putchar('0');
 
 	if ((num / 10) > 0)
 		print_number(num / 10);
 
 	_putchar((num % 10) + '0');
+}
+
+int main(int argc, char** argv)
+{
+	int n = atoi(argv[1]);
+	printnumber(n);
+	return("\n");
 }
