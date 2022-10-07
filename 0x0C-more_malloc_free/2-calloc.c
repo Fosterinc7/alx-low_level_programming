a0da783c8f494f645fcf3af6fa0436a85f4da6cc
fosterinc7 <adenoikiadeniyi0108@gmail.com>
2022-10-07 09:50:47 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,11 +1,11 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * _calloc - allocate memory using malloc and initialize it to zero
  * @nmemb: number of elements
  * @size: size of the memory block to be allocated
  *
- * Return: poiner to the address of the memory block
+ * Return: pointer to the address of the memory block
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
