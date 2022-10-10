bdbdcfa7480fea3c3cae3f8e86fecd060394513f
fosterinc7 <adenoikiadeniyi0108@gmail.com>
2022-10-10 15:58:51 -0700

    Task 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - header file that defines a macro named SIZE as an 
+ * abbreviation for the token 1024.
+ *
+ * Return: ALways 0.
+ */
+
+
