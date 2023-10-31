7c5fc037cba7042bcba22ce33b84f8bd1a149daa
fosterinc7 <adenoikiadeniyi0108@gmail.com>
2023-10-31 15:47:20 -0700

    UWQ

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,8 +15,12 @@
 int linear_search(int *array, size_t size, int value)
 { 
 	size_t i;
+
+	if (!array) 
+		return -1;
 	for (i = 0; i<size ; i++)
 	{
+		printf ("Value checked array[%lu] = [%d]", i, array[i]);
 		if (array[i] == value)
 			return i;
 	}
